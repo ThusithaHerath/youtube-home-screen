2a5caf5b00063b74d5244e026a8eb7f85f57d3c8
Thusitha Herath <thusithalakshanherath@gmail.com>
2024-03-31 21:22:45 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { PageHeader } from './components/PageHeader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="max-h-screen flex-col">
+      <PageHeader/>
+      <div className="">h</div>
+    </h1>
   );
 }
 
